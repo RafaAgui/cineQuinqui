31b6df59a60e690a07a06cbe48e2e561127be85e
Rafael Aguilar Alvear <rafael.aguilara@softtekprojects.com>
2025-09-05 14:04:35 +0200

    feat: add contact with form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,32 @@
+import './Contact.css';
+import { useState } from 'react';
+
 export default function Contact() {
-  return <h1 className="">ℹ️ Contact</h1>;
+
+const [ items, setItems ] = useState ({ });
+
+
+const onSubmitHandler = (e) => {
+  e.preventDefault();
+  localStorage.setItem('contactFormData', JSON.stringify(items));
+  console.log(JSON.stringify(items))
+}
+
+const  onChangeInput = (e) => {
+  const {name, value} = e.target;
+  setItems((prevState) => ({...prevState, [name]: value }))
+}
+
+  return (
+    <>
+      <h1>Contacto</h1>
+      <form action="" className="form" onSubmit={onSubmitHandler}>
+        <input type="text" className="form__input"name="name" placeholder="Nombre" onChange={onChangeInput}/>
+        <input type="text" className="form__input"name="secondname" placeholder="Apellido" onChange={onChangeInput}/>
+        <input type="pass" className="form__input"name="pass" placeholder="pass" onChange={onChangeInput}/>
+
+        <button className="form__button" type="submit">Enviar</button>
+      </form>
+    </>
+  )
 }
